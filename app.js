d3aacf975d3098cedd61fcf8dcd27fdb1fbd39ae
ResmiRamani <Resmi.RamaniAmma@triosstudent.com>
2019-10-15 09:40:32 -0400

    Added : Semicolon

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const fetch = require('node-fetch');
 
 console.log("Hello world");
-console.log("Hai , I am Resmi")
+console.log("Hai , I am Resmi");
 console.log("Welcome");
 fetch('https://google.com')
 .then(
